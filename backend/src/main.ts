7ce9ead8cd20495f188b424c221c6076c6bb6319
Jaime <jaime.edu26@gmail.com>
2026-05-27 03:16:03 -0300

    test: desabilita helmet para testar Render

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
-import helmet from 'helmet';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(helmet());
   app.use(cookieParser());
 
   app.enableCors({
@@ -24,9 +22,7 @@ async function bootstrap() {
   );
 
   const port = process.env.PORT || 10000;
-
-await app.listen(port, '0.0.0.0');
-
-console.log(`🚀 API rodando na porta ${port}`);
+  await app.listen(port, '0.0.0.0');
+  console.log(`🚀 API rodando na porta ${port}`);
 }
-bootstrap();  
+bootstrap();
